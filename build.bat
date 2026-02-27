2f41e7761e1f04d143ab19701c7fb5c6e6a325fd
wuyuetao <yuetao.wu@autoliv.com>
2026-02-27 16:36:09 +0800

    Add icon support: icon.ico + update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-pyinstaller --onefile --windowed --name "ExcelBirthdayConverter" format_birthday_gui.py
+pyinstaller --onefile --windowed --icon "icon.ico" --name "ExcelBirthdayConverter" format_birthday_gui.py
 echo.
 echo Done! Output: dist\ExcelBirthdayConverter.exe
 pause
